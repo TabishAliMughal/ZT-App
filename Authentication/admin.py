83a9c56cf1696c4510c34d03bf0bd5e268e67d0e
TabishAliMughal <tabishalimughal@gmail.com>
2022-02-25 20:24:45 +0500

    abc

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from django.contrib.auth.models import User , Group , GroupManager
 
 
-admin.site.unregister(User)
-admin.site.register(User,ImportExportModelAdmin)
+# admin.site.unregister(User)
+# admin.site.register(User,ImportExportModelAdmin)
 
-admin.site.unregister(Group)
-admin.site.register(Group,ImportExportModelAdmin)
+# admin.site.unregister(Group)
+# admin.site.register(Group,ImportExportModelAdmin)
 
